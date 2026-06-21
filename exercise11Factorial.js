7e872003dd92aebd8c1757c2b9debaac6d0aedcb
Akshat raj singh <akshatrajsingh2022@gmail.com>
2026-06-21 21:35:28 +0530

    factorial

---
+++ exercise11Factorial.js
@@ -0,0 +1,7 @@
+//write a js code for calculating factorial of a number using for loop and reduce function
+let num=5;
+var result=1;
+for(let i=num;i>0;i--){
+    result*=i;
+}
+console.log(result);
